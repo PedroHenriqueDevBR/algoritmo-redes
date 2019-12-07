f19774286de14de6eca1097d1eeb051870775283
john-emerson14 <john.carv.sousa@gmail.com>
2019-12-07 17:41:46 -0300

    refactor: change self lists and input errors

--- CRC/transmissor.py
+++ CRC/transmissor.py
@@ -1,26 +1,30 @@
 class CRC:
-    def __init__(self, tamanho):
-        self.frame_check_sequence = [-1] * tamanho
-        self.final_message = [-1] * tamanho
-        self.generator = [-1] * tamanho
-        self.rest = [-1] * tamanho
+    def __init__(self):
+        self.frame_check_sequence = []
+        self.final_message = []
+        self.generator = []
+        self.rest = []
         
         self.remainder_size = 0
         self.generator_size = 0
         self.message_size = 0
+
 
     def getData(self):
         wrong = False
 
         while True:
             message = input("Enter the message (Ex. 11001): ")
-            for i in range(len(message)):
-                if message[i] not in ['0', '1']:
-                    wrong = True
+            if len(message) == 0:
+                wrong = True
+            else:
+                for i in range(len(message)):
+                    if message[i] not in ['0', '1']:
+                        wrong = True
             if not wrong:
                 self.message_size = len(message)
                 for i in range(self.message_size):
-                    self.final_message[i] = int(message[i])
+                    self.final_message.append(int(message[i]))
                 break
             else:
                 print("Message is wrong!")
@@ -28,19 +32,23 @@ class CRC:
 
         while True:
             gen = input("Enter the generator (Ex. 1011): ")
-            if gen[0] != '1' or gen[-1] != '1':
+            if len(gen) == 0:
                 wrong = True
-            for i in range(len(gen)):
-                if gen[i] not in ['0', '1']:
+            else:
+                if gen[0] != '1' or gen[-1] != '1':
                     wrong = True
+                for i in range(len(gen)):
+                    if gen[i] not in ['0', '1']:
+                        wrong = True
             if not wrong:
                 self.generator_size = len(gen)
                 for i in range(self.generator_size):
-                    self.generator[i] = int(gen[i])
+                    self.generator.append(int(gen[i]))
                 break
             else:
                 print("Generator is wrong!")
                 wrong = False
+
 
     def showData(self):
         print("\nEntered message: ", end='')
@@ -53,6 +61,7 @@ class CRC:
             print(self.generator[i], end='')
         print()
 
+
     def convert(self):
         bits_to_right = 0
         lenght = 0
@@ -60,7 +69,7 @@ class CRC:
         j = 0
 
         for i in range(self.message_size, self.message_size + self.generator_size):
-            self.final_message[i] = 0
+            self.final_message.append(0)
 
         self.message_size = (self.message_size + self.generator_size) - 1
 
@@ -70,7 +79,7 @@ class CRC:
         
         self.remainder_size = self.message_size
         for i in range(self.message_size):
-            self.rest[i] = self.final_message[i]
+            self.rest.append(self.final_message[i])
 
         for i in range(self.generator_size):
             self.rest[i] = self.final_message[i]^self.generator[i]
@@ -78,13 +87,16 @@ class CRC:
 
         while lenght <= self.message_size:
             while True:
-                if self.rest[bits_to_right] == 0:
-                    bits_to_right += 1
-                else:
+                if len(self.rest) == 1:
                     break
+                else:
+                    if self.rest[bits_to_right] == 0:
+                        bits_to_right += 1
+                    else:
+                        break
 
             for i in range(bits_to_right, self.generator_size + bits_to_right):
-                if self.rest[i] != -1:
+                if self.rest[i] == 0 or self.rest[i] == 1:
                     count += 1
 
             print("\nCount: ", count)
@@ -107,25 +119,27 @@ class CRC:
         for i in range(1, self.generator_size):
             self.final_message[self.remainder_size] = self.rest[self.remainder_size]
             self.remainder_size -= 1
+        
+        self.remainder_size = self.message_size - 1
+
+        self.frame_check_sequence = [-1] * (self.generator_size - 1)
+
+        for i in range(len(self.frame_check_sequence) - 1, - 1, - 1):
+            self.frame_check_sequence[i] = self.rest[self.remainder_size]
+            self.remainder_size -= 1
+        
+        print("\n\nframe check sequence: ", end='')
+        for i in range(self.generator_size - 1):
+            print(self.frame_check_sequence[i], end='')
 
         print("\nCRC generated: ", end='')
         for i in range(self.message_size):
             print(self.final_message[i], end='')
-        
-        self.remainder_size = self.message_size - 1
-
-        for i in range(self.generator_size - 1, 0, -1):
-            self.frame_check_sequence[i] = self.rest[self.remainder_size]
-            self.remainder_size -= 1
-        
-        print("\nframe check sequence: ", end='')
-        for i in range(1, self.generator_size):
-            print(self.frame_check_sequence[i], end='')
         print()
 
 
 def main():
-    crc = CRC(50)
+    crc = CRC()
     crc.getData()
     crc.showData()
     crc.convert()
