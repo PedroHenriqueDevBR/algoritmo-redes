8d780e7ef463abbcf1c7e571467f1bb6286c9c64
john-emerson14 <john.carv.sousa@gmail.com>
2019-12-07 16:09:43 -0300

    fix: input errors

--- CRC/transmissor.py
+++ CRC/transmissor.py
@@ -10,15 +10,37 @@ class CRC:
         self.message_size = 0
 
     def getData(self):
-        message = input("Enter the message (Ex. 11001): ")
-        self.message_size = len(message)
-        for i in range(self.message_size):
-            self.final_message[i] = int(message[i])
+        wrong = False
 
-        gen = input("Enter the generator (Ex. 1011): ")
-        self.generator_size = len(gen)
-        for i in range(self.generator_size):
-            self.generator[i] = int(gen[i])
+        while True:
+            message = input("Enter the message (Ex. 11001): ")
+            for i in range(len(message)):
+                if message[i] not in ['0', '1']:
+                    wrong = True
+            if not wrong:
+                self.message_size = len(message)
+                for i in range(self.message_size):
+                    self.final_message[i] = int(message[i])
+                break
+            else:
+                print("Message is wrong!")
+                wrong = False
+
+        while True:
+            gen = input("Enter the generator (Ex. 1011): ")
+            if gen[0] != '1' or gen[-1] != '1':
+                wrong = True
+            for i in range(len(gen)):
+                if gen[i] not in ['0', '1']:
+                    wrong = True
+            if not wrong:
+                self.generator_size = len(gen)
+                for i in range(self.generator_size):
+                    self.generator[i] = int(gen[i])
+                break
+            else:
+                print("Generator is wrong!")
+                wrong = False
 
     def showData(self):
         print("\nEntered message: ", end='')
@@ -62,7 +84,6 @@ class CRC:
                     break
 
             for i in range(bits_to_right, self.generator_size + bits_to_right):
-                # if self.rest[i] == 0 or self.rest[i] == 1:
                 if self.rest[i] != -1:
                     count += 1
 
@@ -100,10 +121,11 @@ class CRC:
         print("\nframe check sequence: ", end='')
         for i in range(1, self.generator_size):
             print(self.frame_check_sequence[i], end='')
+        print()
 
 
 def main():
-    crc = CRC(20)
+    crc = CRC(50)
     crc.getData()
     crc.showData()
     crc.convert()
